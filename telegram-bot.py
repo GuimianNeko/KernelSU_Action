0cf519029cad522461d0781240899e7ca8527229
wstjf <wstjf@qq.com>
2023-04-22 03:15:43 +0800

    test

--- telegram-bot.py
+++
@@ -1,9 +0,0 @@
-import telegram
-
-bot = telegram.Bot(token='1991981456:AAGnXsUtB54L9z_9FeiaBpezU6sAe3uNwvs')
-
-def handle(update, context):
-    print(f'message from {update.message.from_user.first_name}: {update.message.text}')
-    
-bot.message_handle(handle)
-bot.polling()
