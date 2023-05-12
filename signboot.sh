adf0c56ea4c9a447d352c560300151909d6d8bbd
wstjf <wstjf@qq.com>
2023-05-12 22:33:14 +0800

    test

--- signboot.sh
+++ signboot.sh
@@ -1,5 +1,5 @@
 echo $version
-#if [[ "$$version" == *"android12"* ]]; then
+if [[ "$version" == "android12" ]]; then
     echo '[+] Download prebuilt ramdisk'
 	curl -Lo gki-kernel.zip https://dl.google.com/android/gki/gki-certified-boot-android12-5.10-"$patchlevel"_r1.zip
 	unzip gki-kernel.zip && rm gki-kernel.zip
@@ -23,11 +23,11 @@ echo $version
 	echo '[+] Building boot-lz4.img'
 	$MKBOOTIMG --header_version 4 --kernel Image.lz4 --output boot-lz4.img --ramdisk out/ramdisk --os_version 12.0.0 --os_patch_level "$patchlevel"
 	$AVBTOOL add_hash_footer --partition_name boot --partition_size $((64 * 1024 * 1024)) --image boot-lz4.img --algorithm SHA256_RSA2048 --key $GITHUB_WORKSPACE/kernel-build-tools/linux-x86/share/avb/testkey_rsa2048.pem
-#else
-#cp boot.img ../bootdir/
-#$MKBOOTIMG --header_version 4 --kernel Image --output boot1.img
-#$AVBTOOL add_hash_footer --partition_name boot --partition_size $((64 * 1024 * 1024)) --image boot1.img --algorithm SHA256_RSA2048 --key $GITHUB_WORKSPACE/kernel-build-tools/linux-x86/share/avb/testkey_rsa2048.pem
-#fi
+else
+cp boot.img ../bootdir/
+$MKBOOTIMG --header_version 4 --kernel Image --output boot1.img
+$AVBTOOL add_hash_footer --partition_name boot --partition_size $((64 * 1024 * 1024)) --image boot1.img --algorithm SHA256_RSA2048 --key $GITHUB_WORKSPACE/kernel-build-tools/linux-x86/share/avb/testkey_rsa2048.pem
+fi
 cp boot1.img ./bootdir/
 cp boot-lz4.img ./bootdir/
 cp boot-lz4.img ./bootdir/
