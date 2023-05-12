a9372fcdd26ee2df01f325e736db3fab300949fc
wstjf <wstjf@qq.com>
2023-05-12 17:28:29 +0800

    test

--- signboot.sh
+++ signboot.sh
@@ -1,6 +1,6 @@
 if [[ "$${{ inputs.version_name }}" == *"android12"* ]]; then
     echo '[+] Download prebuilt ramdisk'
-	curl -Lo gki-kernel.zip https://dl.google.com/android/gki/gki-certified-boot-android12-5.10-"${ patch_level }"_r1.zip
+	curl -Lo gki-kernel.zip https://dl.google.com/android/gki/gki-certified-boot-android12-5.10-"${ inputs.{ inputs.patch_level } }"_r1.zip
 	unzip gki-kernel.zip && rm gki-kernel.zip
 
 	echo '[+] Unpack prebuilt boot.img'
@@ -12,15 +12,15 @@ if [[ "$${{ inputs.version_name }}" == *"android12"* ]]; then
 	$GZIP -n -k -f -9 Image >Image.gz
 
 	echo '[+] Building boot.img'
-	$MKBOOTIMG --header_version 4 --kernel Image --output boot1.img --ramdisk out/ramdisk --os_version 12.0.0 --os_patch_level "${PATCH_LEVEL}"
+	$MKBOOTIMG --header_version 4 --kernel Image --output boot1.img --ramdisk out/ramdisk --os_version 12.0.0 --os_{ inputs.patch_level } "${{ inputs.patch_level }}"
 	$AVBTOOL add_hash_footer --partition_name boot --partition_size $((64 * 1024 * 1024)) --image boot.img --algorithm SHA256_RSA2048 --key $GITHUB_WORKSPACE/kernel-build-tools/linux-x86/share/avb/testkey_rsa2048.pem
 
 	echo '[+] Building boot-gz.img'
-	$MKBOOTIMG --header_version 4 --kernel Image.gz --output boot-gz.img --ramdisk out/ramdisk --os_version 12.0.0 --os_patch_level "${PATCH_LEVEL}"
+	$MKBOOTIMG --header_version 4 --kernel Image.gz --output boot-gz.img --ramdisk out/ramdisk --os_version 12.0.0 --os_{ inputs.patch_level } "${{ inputs.patch_level }}"
 	$AVBTOOL add_hash_footer --partition_name boot --partition_size $((64 * 1024 * 1024)) --image boot-gz.img --algorithm SHA256_RSA2048 --key $GITHUB_WORKSPACE/kernel-build-tools/linux-x86/share/avb/testkey_rsa2048.pem
 
 	echo '[+] Building boot-lz4.img'
-	$MKBOOTIMG --header_version 4 --kernel Image.lz4 --output boot-lz4.img --ramdisk out/ramdisk --os_version 12.0.0 --os_patch_level "${PATCH_LEVEL}"
+	$MKBOOTIMG --header_version 4 --kernel Image.lz4 --output boot-lz4.img --ramdisk out/ramdisk --os_version 12.0.0 --os_{ inputs.patch_level } "${{ inputs.patch_level }}"
 	$AVBTOOL add_hash_footer --partition_name boot --partition_size $((64 * 1024 * 1024)) --image boot-lz4.img --algorithm SHA256_RSA2048 --key $GITHUB_WORKSPACE/kernel-build-tools/linux-x86/share/avb/testkey_rsa2048.pem
 else
 cp boot.img ../bootdir/
