e4ec4886bca9fa2e767803433345ae2b9817e8e5
wstjf <wstjf@qq.com>
2023-05-12 15:29:19 +0800

    test

---
+++ signboot.sh
@@ -0,0 +1,2 @@
+$MKBOOTIMG --header_version 4 --kernel Image --output boot1.img
+$AVBTOOL add_hash_footer --partition_name boot --partition_size $((64 * 1024 * 1024)) --image boot1.img --algorithm SHA256_RSA2048 --key ../kernel-build-tools/linux-x86/share/avb/testkey_rsa2048.pem
