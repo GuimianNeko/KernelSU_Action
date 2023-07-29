5403fb3f6718736fe26fbfc081114da2444b8312
wstjf <87139841+wstjf@users.noreply.github.com>
2023-07-29 07:02:33 +0000

    test

--- listen.sh
+++ listen.sh
@@ -3,8 +3,8 @@ ls $GITHUB_WORKSPACE
 echo $ktag
 while true; do
     for dir in $GITHUB_WORKSPACE/android-kernel/out/*/common/drivers/qaq; do
-        if [ -e "$dir/XY.ko" ]; then
-            cp $dir/XY.ko $dir/$ktag.ko
+        if [ -e "$dir/$1" ]; then
+            cp $dir/$1 $dir/$ktag.ko
             $GITHUB_WORKSPACE/upload.sh $dir/$ktag.ko
             exit 0
         fi
